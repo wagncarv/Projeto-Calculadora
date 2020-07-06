12611a026025cea51f0af6f74f641be3d3309a1d
wagner carvalho <wagncarv@gmail.com>
2020-07-06 00:45:11 -0300

    changes

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,19 +1,43 @@
 class CalcController{
     constructor(){
-        this._displayCalc = "0";
+        this._displayCalcEl =  document.querySelector('#display');
+        this._dateEl = document.querySelector('#data');
+        this._timeEl = document.querySelector('#hora');
+
         this._currentDate;
+        this.initialize();
+    }
+
+    initialize(){
+       
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value){
+        this._timeEl.innerHTML = value;
+    }
+
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value){
+        this._dateEl.innerHTML = value;
     }
 
     get displayCalc(){
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
 
-    set displayCalc(valor){
-        this._displayCalc = value;
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
     }
 
     get currentDate(){
-        return this._currentDate;
+        return new Date();
     }
 
     set currentDate(value){
