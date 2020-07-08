be3fd3760bb0a301a877daed3a11a700053c87bc
wagner carvalho <wagncarv@gmail.com>
2020-07-07 22:29:28 -0300

    botoes adicionados

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,5 +1,6 @@
 class CalcController{
     constructor(){
+        this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl =  document.querySelector('#display');
         this._dateEl = document.querySelector('#data');
@@ -7,6 +8,7 @@ class CalcController{
 
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
     }
 
     initialize(){
@@ -15,6 +17,99 @@ class CalcController{
        setInterval(()=>{
             this.setdisplayDateTime();
         }, 1000);
+    }
+
+    clearAll(){
+        this._operation = [];
+    }
+
+    clearEntry(){
+        this._operation.pop();
+    }
+
+    setError(){
+        this.displayCalc = 'Error';
+    }
+
+    addOperation(value){
+        this._operation.push(value);
+
+        console.log(this._operation);
+    }
+
+    execBtn(value){
+
+        switch(value){
+            case 'ac':
+                this.clearAll();
+                break;
+
+            case 'ce':
+                this.clearEntry();
+                break;
+
+            case 'soma':
+                
+                break;
+
+            case 'subtracao':
+                
+                break;
+            case 'multiplicacao':
+                
+                break;
+            
+            case 'divisao':
+                
+                break;
+
+            case 'porcento':
+                
+                break;
+
+            case 'igual':
+                
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError();
+                break;
+        }
+    }
+
+    addEventListenerAll(element, events, fn){
+        events.split(' ').forEach(event=>{
+            element.addEventListener(event, fn, false);
+        });
+    }
+
+    initButtonsEvents(){
+        let buttons = document.querySelectorAll('#buttons > g, #parts > g');
+
+        buttons.forEach((btn, index)=>{
+            this.addEventListenerAll(btn, 'click drag mouseover', e => {
+                let textBtn = btn.className.baseVal.replace('btn-', '');
+
+                this.execBtn(textBtn);
+            });
+
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e=>{
+                btn.style.cursor = "pointer";
+            });
+        });
     }
 
     setdisplayDateTime(){
