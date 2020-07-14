c778445475acbcae143d5e1af1a2c53d2adfd648
wagner carvalho <wagncarv@gmail.com>
2020-07-13 22:57:23 -0300

    evento áudio, tratamento de exceções

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,5 +1,7 @@
 class CalcController{
     constructor(){
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
 
@@ -12,6 +14,24 @@ class CalcController{
         this._currentDate;
         this.initialize();
         this.initButtonsEvents();
+        this.initKeyBoard();
+    }
+
+    copyToClipBoard(){
+        let input = document.createElement('input');
+        input.value = this.displayCalc;
+        document.body.appendChild(input);
+        input.select();
+        document.execCommand("Copy");
+
+        input.remove();
+    }
+
+    pasteFromClipboard(){
+        document.addEventListener('paste', e=>{
+            let text = e.clipboardData.getData('Text');
+            this.displayCalc = parseFloat(text);
+        });
     }
 
     initialize(){
@@ -22,6 +42,76 @@ class CalcController{
         }, 1000);
 
         this.setLastNumberToDisplay();
+        this.pasteFromClipboard();
+
+        document.querySelectorAll('.btn-ac').forEach(btn=>{
+            btn.addEventListener('dblclick', e=>{
+                this.toggleAudio();
+            });
+        });
+    }
+
+    toggleAudio(){
+        this._audioOnOff = !this._audioOnOff;
+    }
+
+    playAudio(){
+        if (this._audioOnOff){
+            this._audio.currentTime = 0;
+            this._audio.play();
+        }
+    }
+
+    initKeyBoard(){
+        document.addEventListener('keyup', e=>{
+
+            this.playAudio();
+
+            switch(e.key){
+                case 'Escape':
+                    this.clearAll();
+                    break;
+    
+                case 'Backspace':
+                    this.clearEntry();
+                    break;
+    
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '%':
+                    this.addOperation(e.key);
+                    break;
+    
+                case 'Enter':
+                case '=':
+                    this.calc();
+                    break;
+    
+                case '.':
+                case ',':
+                    this.addDot();
+                    break;
+    
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(e.key);
+                    break;
+
+                case 'c':
+                    if(e.ctrlKey) this.copyToClipBoard();
+                    break;
+            }
+        });
     }
 
     clearAll(){
@@ -129,7 +219,14 @@ class CalcController{
     }
 
     getResult(){
-        return eval(this._operation.join(""));
+        try{
+            return eval(this._operation.join(""));
+        }catch(e){
+            setTimeout(()=>{
+                this.setError();
+            }, 1);
+        }
+        
     }
 
     getLastItem(isOperator = true){
@@ -163,6 +260,7 @@ class CalcController{
     }
 
     execBtn(value){
+        this.playAudio();
 
         switch(value){
             case 'ac':
@@ -230,7 +328,7 @@ class CalcController{
         let buttons = document.querySelectorAll('#buttons > g, #parts > g');
 
         buttons.forEach((btn, index)=>{
-            this.addEventListenerAll(btn, 'click', e => {
+            this.addEventListenerAll(btn, 'click drag', e => {
                 let textBtn = btn.className.baseVal.replace('btn-', '');
 
                 this.execBtn(textBtn);
@@ -272,6 +370,10 @@ class CalcController{
     }
 
     set displayCalc(value){
+        if (value.toString().length > 10){
+            this.setError();
+            return;
+        }
         this._displayCalcEl.innerHTML = value;
     }
 
